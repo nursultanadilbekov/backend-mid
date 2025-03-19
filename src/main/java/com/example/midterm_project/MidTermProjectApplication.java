ae0274cdd76d9f020b6b529cb1bc37a416fb68a3
nursultan <nursultanadilbekov5@gmail.com>
2025-03-19 17:48:17 +0600

    first commit

---
+++ src/main/java/com/example/midterm_project/MidTermProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.midterm_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidTermProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MidTermProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/midterm_project/MidTermProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.midterm_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidTermProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
